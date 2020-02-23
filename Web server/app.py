43a4c09284894b59d946f285c9c60783bb8b1ba8
Enrico Blackwell <pumpkintitan11@gmail.com>
2020-02-23 02:57:11 -0700

    IT WORKS

--- Web server/app.py
+++ Web server/app.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from markupsafe import escape
+import twitter
+
+auth = open("../twitterauth")
+
+api = twitter.Api(consumer_key=auth.readline().strip(),
+                  consumer_secret=auth.readline().strip(),
+                  access_token_key=auth.readline().strip(),
+                  access_token_secret=auth.readline().strip())
 
 app = Flask(__name__)
 
+uname = ""
 
-@app.route('/<username>')
-def show_user_profile(username):
+
+@app.route('/<location>')
+def grab_location(location):
     # show the user profile for that user
-    print("Hit", username)
-    return '%s' % escape(username)
+    status = api.PostUpdate("The user is currently at: ", location)
+    return '%s' % escape(location)
+
 
 app.run(host="0.0.0.0", port=5002)
