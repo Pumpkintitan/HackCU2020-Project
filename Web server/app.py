2c862045e0c52a33331b08cae8b00fcf88659cf0
Enrico Blackwell <pumpkintitan11@gmail.com>
2020-02-22 22:20:28 -0700

    eugh:

--- Web server/app.py
+++ Web server/app.py
@@ -4,14 +4,10 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
 @app.route('/<username>')
 def show_user_profile(username):
     # show the user profile for that user
+    print("Hit", username)
     return '%s' % escape(username)
 
 app.run(host="0.0.0.0", port=5002)
